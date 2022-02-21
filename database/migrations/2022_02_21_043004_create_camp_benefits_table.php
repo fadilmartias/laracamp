e85a37283dc9dd3a64553f7ec67df70c1cc25f68
fadilmartias <fadilmartias26@gmail.com>
2022-02-21 14:54:09 +0700

    migration table

---
+++ database/migrations/2022_02_21_043004_create_camp_benefits_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            // list method
+            // $table->unsignedBigInteger('camp_id');
+            // $table->bigInteger('camp_id')->unsigned();
+
+            // 2nd method
+            $table->foreignId('camp_id')->constrained();
+
+            $table->string('name');
+            $table->timestamps();
+
+            // list method
+            // $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+};
