80fcab6a7201c98ee8910ef82451a0dded31e87d
fadilmartias <fadilmartias26@gmail.com>
2022-02-21 23:35:46 +0700

    slicing login, checkout dan success_checkout

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('login', function () {
+    return view('login');
+})->name('login'); // untuk dipanggil di view pake {{ route('login') }}
+
+Route::get('checkout', function () {
+    return view('checkout');
+})->name('checkout'); // untuk dipanggil di view pake {{ route('checkout') }}
+
+Route::get('success-checkout', function () {
+    return view('success_checkout');
+})->name('success-checkout');
