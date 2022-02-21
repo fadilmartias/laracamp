374497e41cd3aaca0c9ffb9e11df8a946c0d0cb8
fadilmartias <fadilmartias26@gmail.com>
2022-02-21 14:54:21 +0700

    Seeders

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@laracamp.com',
+            'email_verified_at' => date('Y-m-d H:i:s', time()),
+            'password' => bcrypt('password'),
+            'is_admin' => true
+        ]);
+    }
+}
---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Camp;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Gila Belajar',
+                'slug' => 'gila-belajar',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s', time()), // harus ada ini kalo mau pake 2nd method
+                'updated_at' => date('Y-m-d H:i:s', time()), // harus ada ini kalo mau pake 2nd method
+            ],
+            [
+                'title' => 'Baru Mulai',
+                'slug' => 'baru-mulai',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()), // harus ada ini kalo mau pake 2nd method
+                'updated_at' => date('Y-m-d H:i:s', time()), // harus ada ini kalo mau pake 2nd method
+            ],
+        ];
+
+        // 1st method
+        // foreach ($camps as $key => $camp) {
+        //     Camp::create($camp);
+        // }
+
+        // 2nd method
+        Camp::insert($camps);
+    }
+}
