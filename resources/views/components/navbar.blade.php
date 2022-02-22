953dc2b1db7ab0c1eb37493b58b53808698e9651
fadilmartias <fadilmartias26@gmail.com>
2022-02-22 18:18:07 +0700

    install and integration socialite

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -21,6 +21,25 @@
                     <a class="nav-link" href="#">Business</a>
                 </li>
             </ul>
+            @auth
+            <div class="d-flex user-logged nav-item dropdown no-arrow">
+                <a href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                    Halo, {{ Auth::user()->name }}!
+                    <img src="{{ Auth::user()->avatar }}" class="user-photo rounded-circle" alt="">
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink" style="right: 0; left: auto">
+                        <li>
+                            <a href="#" class="dropdown-item">My Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="#" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit()">Sign Out</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            </form>
+                        </li>
+                    </ul>
+                </a>
+            </div>
+            @else
             <div class="d-flex">
                 <a href="{{ route('login') }}" class="btn btn-master btn-secondary me-3">
                     Sign In
@@ -29,6 +48,8 @@
                     Sign Up
                 </a>
             </div>
+            @endauth
+
         </div>
     </div>
 </nav>
